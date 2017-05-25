8b83a6b79297acd33edf9ce54e97e2d66b285af5
mcale <mcale@HomeAway.com>
2017-05-25 16:49:42 -0500

    explanation added

--- linear-regression/least-square/comprehension/linear-regression.js
+++ linear-regression/least-square/comprehension/linear-regression.js
@@ -1,4 +1,5 @@
-// y = b0 * (b1 * avg(xValues))
+// https://en.wikipedia.org/wiki/Least_squares
+// https://www.youtube.com/watch?v=JvS2triCgOY&t=304s
 
 const average = require('./average.js');
 const distanceToMean = require('./distance-to-mean.js');
@@ -6,7 +7,6 @@ const squareDistance = require('./square-distance.js');
 const multiplyMeans = require('./multiply-means.js');
 const calculateB1 = require('./calculate-b1.js');
 const calculateB0 = require('./calculate-b0.js');
-
 
 const observations = [
   {"x" :  1, "y" : 2},
@@ -16,29 +16,28 @@ const observations = [
   {"x" :  5, "y" : 5}
 ]
 
-// Create averages for each axis
+// Calculate mean for each observation axis
 let xAverage = average(observations, 'x');
 let yAverage = average(observations, 'y');
 
+// Calculate each observation's distance from its axises mean
 let observationsWithDistancesXOnly = distanceToMean(observations, 'x', xAverage);
 let observationsWithDistances = distanceToMean(observationsWithDistancesXOnly, 'y', yAverage);
 
-// Now that we have our distance from the mean data in our table we can square the indepedent (x axis) values
+// Calculate the squared distance for indepedent
 let observationsWithDistanceSquares = squareDistance(observationsWithDistances);
 
-// In addition we need to find the product of multiplying both means together
+// Calculate the product of each observations distance from the mean
 let finalProductsTable = multiplyMeans(observationsWithDistanceSquares);
-// console.log(finalProductsTable);
 
-
+// Calculate b1 value
 let b1 =  calculateB1(finalProductsTable);
-
 console.log(b1);
 
+// Calculate b0 value
 let b0 = calculateB0(xAverage, yAverage, b1);
-
 console.log(b0);
 
+// Calculate FINAL_SOLUTION
 let FINAL_SOLUTION =  b0 + (b1 * xAverage);
-
 console.log(FINAL_SOLUTION);
